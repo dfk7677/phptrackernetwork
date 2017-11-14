800b2a5bb36e261948f7c18f679ecfeca0bf0c49
Dionysis Kapatsoris <dfk_7677@yahoo.com>
2017-11-14 16:54:40 +0200

    Added handleException function

--- src/BFTracker.php
+++ src/BFTracker.php
@@ -2,10 +2,13 @@
 /**
  * BFTracker.php
  *  
+ * PHP version 7.1
+ * 
  * @category API_Wrapper
- * @package  BFTracker
+ * @package  Phptrackernetwork
  * @author   Dionysis Kapatsoris <dfk_7677@yahoo.com>
  * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/dfk7677/phptrackernetwork
  */
 
 namespace dfk7677\phptrackernetwork;
@@ -21,6 +24,7 @@ use GuzzleHttp\Psr7\Response;
  * @package  BFTracker
  * @author   Dionysis Kapatsoris <dfk_7677@yahoo.com>
  * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/dfk7677/phptrackernetwork
  */
 
 class BFTracker
@@ -36,8 +40,10 @@ class BFTracker
      * @param string $apiKey  Tracker Network API key
      * @param string $baseUri Base URI for Battlefield stats API calls
      */
-    public function __construct($apiKey, $baseUri = "https://battlefieldtracker.com/bf1/api/Stats/")
-    {
+    public function __construct(
+        $apiKey, 
+        $baseUri = "https://battlefieldtracker.com/bf1/api/"
+    ) {
         $this->_baseUri = $baseUri;
         $this->_client = new Client;
         $this->_apiKey = $apiKey;
@@ -54,21 +60,77 @@ class BFTracker
      * 
      * @return object
      */
-    public function getBasicStatsByNickname($nickname, $platform = 3, $game = "tunguska")
-    {
-        $path = "BasicStats";
+    public function getBasicStatsByNickname(
+        $nickname,
+        $platform = 3,
+        $game = "tunguska"
+    ) {
+        $path = "Stats/BasicStats";
         try {
             $response = $this->_client->request(
                 'GET',
                 $this->_baseUri.$path,
                 [
                     'headers' => ['TRN-Api-Key' => $this->_apiKey],
-                    'query' => ['platform' => $platform, 'game' => $game, 'displayName' => $nickname]
+                    'query' => [
+                        'platform' => $platform,
+                        'game' => $game,
+                        'displayName' => $nickname
+                        ]
                 ]
             ); 
             return json_decode($response->getBody()->getContents());
         } catch (ClientException $e) {
-            $content = $e->getResponse()->getBody()->getContents();
+            return $this->_handleException($e);
+        }       
+    }
+
+    /** 
+     * GetBasicStatsById function
+     * Returns Basic Stats for player by his Origin ID
+     * 
+     * @param integer $id       Origin ID
+     * @param integer $platform Platform ID
+     * @param string  $game     Game codename
+     * 
+     * @return object
+     */
+    public function getBasicStatsById(
+        $id,
+        $platform = 3,
+        $game = "tunguska"
+    ) {
+        $path = "Stats/BasicStats";
+        try {            
+            $response = $this->_client->request(
+                'GET',
+                $this->_baseUri.$path,
+                [
+                    'headers' => ['TRN-Api-Key' => $this->_apiKey],
+                    'query' => ['platform' => $platform,
+                    'game' => $game,
+                    'personaId' => $id
+                    ]
+                ]
+            );            
+            return json_decode($response->getBody()->getContents());            
+        } catch (ClientException $e) {
+            return $this->_handleException($e);           
+        }
+    }
+
+    /** 
+     * Function handleException
+     * Returns object response depending on response exception
+     * 
+     * @param ClientException $exception Guzzle thrown exception
+     * 
+     * @return object 
+     */
+    private function _handleException($exception)
+    {
+        if ($exceptione->hasResponse()) {                
+            $content = $exception->getResponse()->getBody()->getContents();
             $object = json_decode($content);
             if (json_last_error() != JSON_ERROR_NONE) {
                 $object = json_decode(
@@ -82,51 +144,16 @@ class BFTracker
                 );
             }
             return $object;
-        }
-       
-    }
-
-    /** 
-     * GetBasicStatsById function
-     * Returns Basic Stats for player by his Origin ID
-     * 
-     * @param integer $id       Origin ID
-     * @param integer $platform Platform ID
-     * @param string  $game     Game codename
-     * 
-     * @return object
-     */
-    public function getBasicStatsById($id, $platform = 3, $game = "tunguska")
-    {
-        $path = "BasicStats";
-        try {            
-            $response = $this->_client->request(
-                'GET',
-                $this->_baseUri.$path,
-                [
-                    'headers' => ['TRN-Api-Key' => $this->_apiKey],
-                    'query' => ['platform' => $platform, 'game' => $game, 'personaId' => $id]
-                ]
-            );            
-            return json_decode($response->getBody()->getContents());            
-        } catch (ClientException $e) {
-            if ($e->hasResponse()) {                
-                $content = $e->getResponse()->getBody()->getContents();
-                $object = json_decode($content);
-                if (json_last_error() != JSON_ERROR_NONE) {
-                    $object = json_decode(
-                        '{
-                            "successful": false,
-                            "errorData": {
-                                "errorMessage": "Incorrect API key.",
-                                "errorCode": 1
-                            }
-                        }'
-                    );
-                }
-                return $object;
-            }
-           
+        } else {
+            return json_decode(
+                '{
+                    "successful": false,
+                    "errorData": {
+                        "errorMessage": "Unknown exception.",
+                        "errorCode": 3
+                    }
+                }'
+            );
         }
     }
 
