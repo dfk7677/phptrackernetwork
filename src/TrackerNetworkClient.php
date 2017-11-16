c84fe281cced631b1ab14bf9ffaf456a0006f14c
Dionysis Kapatsoris <dfk_7677@yahoo.com>
2017-11-16 15:33:34 +0200

    Class reorganising

---
+++ src/TrackerNetworkClient.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * BFTracker.php.
+ *
+ * PHP version 7.1
+ *
+ * @author  Dionysis Kapatsoris <dfk_7677@yahoo.com>
+ * @license https://opensource.org/licenses/MIT MIT
+ *
+ * @link    https://github.com/dfk7677/phptrackernetwork
+ */
